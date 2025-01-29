c3eedc6b44367519b6055bcdf7366b0a15efb17d
Mingwei Samuel <mingwei.samuel@gmail.com>
2025-01-29 11:23:04 -0800

    chore(dfir_rs): remove unused cross_join, fix #1616 (#1688)

--- dfir_rs/src/compiled/pull/mod.rs
+++ dfir_rs/src/compiled/pull/mod.rs
@@ -1,9 +1,6 @@
 //! Pull-based operator helpers, i.e. [`Iterator`] helpers.
 #![allow(missing_docs, reason = "// TODO(mingwei)")]
 
-mod cross_join;
-pub use cross_join::*;
-
 mod symmetric_hash_join;
 pub use symmetric_hash_join::*;
 
