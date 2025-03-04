4e0c08fb3243c4becbfae2c1e3a2873392fca01d
Mingwei Samuel <mingwei.samuel@gmail.com>
2025-03-03 16:57:27 -0800

    ci: fix `build_docs.bash` after rustup 1.28.0 (#1756)

--- build_docs.bash
+++ build_docs.bash
@@ -7,6 +7,7 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source "$HOME/.cargo/env"
 
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
+rustup toolchain install
 
 cd website_playground
 
